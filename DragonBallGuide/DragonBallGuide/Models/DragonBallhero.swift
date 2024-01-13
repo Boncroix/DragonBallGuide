edbd056e4850021d0993df5de9a536cb5eadee1c
Boncroix <boncroix@gmail.com>
2024-01-13 08:05:26 +0100

    Añadir modelo Hero y transformation

---
+++ DragonBallGuide/DragonBallGuide/Models/DragonBallhero.swift
@@ -0,0 +1,15 @@
+//
+//  DragonBallhero.swift
+//  DragonBallGuide
+//
+//  Created by Jose Bueno Cruz on 13/1/24.
+//
+
+import Foundation
+
+
+struct DragonBallHeroElement: Codable {
+    let photo: String
+    let name, description: String
+    let favorite: Bool
+}
---
+++ DragonBallGuide/DragonBallGuide/Models/HeroTransformation.swift
@@ -0,0 +1,15 @@
+//
+//  HeroTransformation.swift
+//  DragonBallGuide
+//
+//  Created by Jose Bueno Cruz on 13/1/24.
+//
+
+import Foundation
+
+
+struct HeroTransformation: Codable {
+    let id, description: String
+    let photo: String
+    let name: String
+}
